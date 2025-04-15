c047f146df7d680b63015cd55d64d62d73cbf564
JeongAaron <lovejsk1004@gmail.com>
2025-04-15 19:42:45 +0900

    2025-04-15 애니메이션

--- Game/Assets/Class03th(Life Cycle)/Scripts/State.cs
+++ Game/Assets/Class03th(Life Cycle)/Scripts/State.cs
@@ -22,4 +22,35 @@ public class State : MonoBehaviour
         //  게임 오브텍트가 생성되었을 때 단 한번만 호출되며, 스크립트가 비활성화된 상태일 땐, 호출되지 않는 이벤트 함수
         Debug.Log("Start");
     }
+    private void FixedUpdate()
+    {
+        //  FixedUpdate 함수란? 회전 이동 등 물리적 처리
+        //  TimeStep에 설정된 값에 따라 일정한 간격으로 호출되는 이벤트 함수
+        Debug.Log("FixedUpdate");
+        
+    }
+    private void Update()
+    {
+        //  Update 함수란? 키 입력등을 처리
+        //  게임이 실행되는 동안 매 프레임마다 호출되는 이벤트 함수
+        Debug.Log("Update");
+    }
+    private void LateUpdate()
+    {
+        //  LateUpdate 함수란? 캐릭터 이동 후 따라가는 카메라 등을 처리할 때
+        //  Update 함수가 호출된 이후에 호출되는 이벤트 함수
+        Debug.Log("LateUpdate");
+    }
+    private void OnDisable()
+    {
+        //  OnDisable 함수란? OnEnable 함수와 같이 사용되며 오브젝트의 메모리 할당에서 메모리 파편화를 막기위해 Destroy 된 것 처럼 보이게 하기위해 사용
+        //  게임 오브젝트가 비활성화되었을 때 호출되는 이벤트 함수
+        Debug.Log("OnDisable");
+    }
+    private void OnDestroy()
+    {
+        //  OnDestory 함수란?
+        //  게임 오브젝트가 삭제되었을 때 호출되는 이벤트 함수
+        Debug.Log("OnDestroy");
+    }
 }
