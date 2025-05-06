2b0f42b854e147606d7372d701f8b98878497d6f
JeongAaron <lovejsk1004@gmail.com>
2025-05-06 19:41:59 +0900

    2025-05-06 레이 캐스트

---
+++ Game/Assets/Class12th(Ray Cast)/Scripts/Mouse.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Mouse : MonoBehaviour
+{
+    [SerializeField] Ray ray;
+    [SerializeField] RaycastHit raycastHit;
+    void Update()
+    {
+        if(Input.GetMouseButtonDown(0))
+        {
+            ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            if(Physics.Raycast(ray, out raycastHit, Mathf.Infinity))
+            {
+                Debug.Log(raycastHit.collider.gameObject.name);
+            }
+        }
+    }
+}
