b8976ad95a9713731501efb3e9f865b2f3bca669
JeongAaron <lovejsk1004@gmail.com>
2025-05-07 19:41:53 +0900

    2025-05-07 피직스 머티리얼

--- Game/Assets/Class12th(Ray Cast)/Scripts/Mouse.cs
+++ Game/Assets/Class12th(Ray Cast)/Scripts/Mouse.cs
@@ -1,19 +1,32 @@
 using System.Collections;
 using System.Collections.Generic;
+using UnityEditor;
 using UnityEngine;
 
 public class Mouse : MonoBehaviour
 {
     [SerializeField] Ray ray;
-    [SerializeField] RaycastHit raycastHit;
+    [SerializeField] RaycastHit rayCastHit;
+    [SerializeField] LayerMask layerMask;
     void Update()
     {
         if(Input.GetMouseButtonDown(0))
         {
             ray = Camera.main.ScreenPointToRay(Input.mousePosition);
-            if(Physics.Raycast(ray, out raycastHit, Mathf.Infinity))
+            if(Physics.Raycast(ray, out rayCastHit, Mathf.Infinity,layerMask))
             {
-                Debug.Log(raycastHit.collider.gameObject.name);
+
+                GameObject prefab = rayCastHit.collider.gameObject;
+                Debug.Log(prefab.gameObject.name);
+                if(prefab.transform.GetChild(0).gameObject.activeSelf)
+                {
+                    prefab.transform.GetChild(0).gameObject.SetActive(false);
+                    
+                }
+                else
+                {
+                    prefab.transform.GetChild(0).gameObject.SetActive(true);
+                }
             }
         }
     }
